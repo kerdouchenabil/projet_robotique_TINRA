176ce16a4baddf238a90e6579b0308f6684ac19e
assirembouyoucef <60555403+assirembouyoucef@users.noreply.github.com>
2020-02-12 11:50:51 +0100

    Add files via upload

--- test.py
+++ test.py
@@ -1,18 +1,23 @@
-from Robot import Robot
-import math
+from tkinter import *
+from Arene import Arene
+def mouvement():
+    global tourner
+    canvas.move(rob1,ar.r1.dx,ar.r1.dy)
+    ar.r1.avance()
+    if ar.r1.x > 460 and ar.r1.dx == 1:
+        ar.r1.tournerDroite()
+        print("abouch")
+    print(ar.r1.x)
+    tk.after(10,mouvement)
 
-rob = Robot(0,0,0,20,1)
+ar = Arene(500, 500)
+ar.r1.affiche()
+tk = Tk()
+canvas =Canvas(tk,width = ar.dimx,height = ar.dimy,bg='blue')
+canvas.pack(padx = 50, pady = 50)
+rob1=canvas.create_rectangle(ar.r1.getX(),ar.r1.getY(),ar.r1.getX()+30,ar.r1.getY()+20,fill="red")
+tourner = False
+mouvement()
 
-rob.printAll()
+tk.mainloop()
 
-rob.accelerate(2,1)
-
-rob.printAll()
-
-rob.turn_left(math.pi/2)
-
-rob.printAll()
-
-rob.move(1)
-
-rob.printAll()
