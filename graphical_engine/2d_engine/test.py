14da50e0d912fa1684320b62765776e0f4d429cb
Raxa04 <raxa04@gmail.com>
2020-02-19 15:55:35 +0100

    fonction draw pour cree la fenetre

--- graphical_engine/2d_engine/test.py
+++ graphical_engine/2d_engine/test.py
@@ -2,5 +2,24 @@ from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
 
-print 'coucou'
+print('coucou')
 
+window = 0
+width, height = 500, 400
+
+
+def draw():                                 #fonction de base qui va tout dessiner
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glLoadIdentity()
+
+
+    glutSwapBuffers()
+
+glutInit()
+glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH)
+glutInitWindowSize(width, height)
+glutInitWindowPosition(0, 0)
+window = glutCreateWindow("test quelquonque")
+glutDisplayFunc(draw)
+glutIdleFunc(draw)
+glutMainLoop()
