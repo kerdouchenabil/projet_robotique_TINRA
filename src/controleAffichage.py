91914fbf9aa2c7d1fbacb246db2eb33b62f7fc88
Ingrid <ingrid_theret@hotmail.fr>
2020-02-26 22:01:48 +0100

    modification de l'affichage: creation d'une classe pour l'affichage arene + premier test avec controleAffichage

---
+++ src/controleAffichage.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+
+from graphical_engine.engine2D.affichageArene import *
+from objet.arene import *
+from objet.robot import *
+from objet.obstacle import *
+
+     
+arene1= Arene(400, 400)
+
+rob= Robot(0,200,200,0,10)
+arene1.addRobot(rob)
+
+mur0= Obstacle(10,200,20,400) #g
+mur1= Obstacle(200,10,400,20) #b
+mur2= Obstacle(390,200,20,400) #d
+mur3= Obstacle(200,390,400,20) #h
+
+arene1.addObstacle(mur0)
+arene1.addObstacle(mur1)
+arene1.addObstacle(mur2)
+arene1.addObstacle(mur3)
+
+arene1.printAll()
+
+
+
+affich= Affichage2D(arene1)
+
+glutMainLoop()  
+    
+
+
+	
+
+
+#print("affichage2D: longueur=", affich.width, "largeur=", affich.height)
+
+       
+
+        
+        
---
+++ src/graphical_engine/engine2D/affichageArene.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+
+from objet.arene import *
+from objet.robot import *
+from objet.obstacle import *
+
+class Affichage2D:
+
+	def __init__(self, arene):
+		self.arene = arene
+		self.window = 0
+		self.width = arene.largeur
+		self.height = arene.longueur
+
+		# initialisation
+		glutInit()
+
+		#parametre pour l'ouverture de la feneter
+		glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH)
+		glutInitWindowSize(self.width, self.height)
+		glutInitWindowPosition(0, 0)
+
+		#creation de la fenetre
+		glutCreateWindow("Affichage2D")
+		glutDisplayFunc(self.draw)
+		glutIdleFunc(self.animation)
+		
+        
+
+
+        
+	def drawRect(self, x, y, wdth, hght):
+		glBegin(GL_QUADS)                       #signaler a opengl que on va dessiner un rectangle (GL_QUADS)
+		glVertex2f(x - wdth/2, y - hght/2)                        #dessine les 4 vectrices
+		glVertex2f(x + wdth/2, y - hght/2)
+		glVertex2f(x + wdth/2, y + hght/2)
+		glVertex2f(x - wdth/2, y + hght/2)
+		glEnd()                                 #signale la fin du dessin
+
+    
+	def refresh2d(self):
+		glViewport(0, 0, self.width, self.height)
+		glMatrixMode(GL_PROJECTION)
+		glLoadIdentity()
+		glOrtho(0.0, self.width, 0.0, self.height, 0.0, 1.0)
+		glMatrixMode (GL_MODELVIEW)
+		glLoadIdentity()
+
+
+	def drawRobot(self,rob):
+		self.drawRect(rob.posx,rob.posy, 20, 20)
+
+	def drawObstacle(self,obst):
+		self.drawRect(obst.x0, obst.y0, obst.dimx, obst.dimy)
+
+	def draw(self):
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+		glLoadIdentity()
+		self.refresh2d()
+        
+		#dessin de l'Arene avec les obstacles
+		glColor3f(1.0, 0.0, 0.0)
+		for obst in self.arene.listeObst:
+			self.drawObstacle(obst)
+        
+		#dessin du robot
+		glColor3f(1.0, 1.0, 1.0)
+		for rob in self.arene.listeRobot:
+			self.drawRobot(rob)
+
+		glutSwapBuffers() 
+
+
+	def animation(self):
+		rob = self.arene.listeRobot[0]
+		mur0= self.arene.listeObst[0]
+		mur1= self.arene.listeObst[1]
+		mur2= self.arene.listeObst[2]
+		mur3= self.arene.listeObst[3]
+		pcd=rob.possibleCollision(mur2,20,20)
+		pcb=rob.possibleCollision(mur1,20,20)
+		pcg=rob.possibleCollision(mur0,20,20)
+		pch=rob.possibleCollision(mur3,20,20)
+
+		rob.update(pcg,pcd,pch,pcb)	
+		
+		glutPostRedisplay()	
+
+
+
+
+       
+
+        
+        
