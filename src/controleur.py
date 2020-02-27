576eb531181c41e1780026e75caef9e1e3347415
kerdouchenabil <kerdouchenabil@gmail.com>
2020-02-27 02:32:21 +0100

    ajout des methodes de base (a realiser) pour le controleur

--- src/controleur.py
+++ src/controleur.py
@@ -1,6 +1,45 @@
 # -*- coding: utf-8 -*-
 
-class controleur:
+from objet.robot import *
+
+class Controleur:
 	'''
 		le controleur (IA) du robot
 	'''
+	
+	def __init__(self, robot):
+		"""
+			prend le robot en param
+		"""
+		
+		self.robot = robot
+		
+
+	def update(self):
+		"""
+			mise a jour du comportement et de l'etat du robot
+			en fonction de son etat precedent
+		"""
+		
+		#a completer
+		return
+	
+	
+	def move_strategy(self.robot):
+		"""
+			strategie d'avancement du robot (vitesse, accelereation) ?
+		"""
+		
+		#a completer
+		return 
+
+
+	def turn_strategy(self.robot):
+		"""
+			strategie de rotation du robot (degré de rotation) ?
+		"""
+		
+		#a completer
+		return 
+	
+	
