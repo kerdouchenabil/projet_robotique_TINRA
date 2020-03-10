05b24937a7daaf39bcac859e6f17d5995e57c6e3
kerdouchenabil <kerdouchenabil@gmail.com>
2020-03-10 02:08:53 +0100

    mise a jour de la doc controleur (en attente des docs de quelque fonctions)

--- src/controleur/ai.py
+++ src/controleur/ai.py
@@ -6,11 +6,24 @@ from objet.robot import *
 class Controleur:
 	'''
 		le controleur (IA) du robot
+		utilise les fonctions de commande du robot: (possibilité de passer par un proxy)
+		set_motor_dps(id, dps)
+		get_position(id)
+		get_distance()
+		...
+		ces fonctions sont utilisées pour implementer de petites strategies basiques comme
+		stop_strategy()
+		turnRight_strategy
+		turnLeft_strategy
+		...
+		ces strategies de base sont utilisées pour implementer les fonctionnalitées attendues (strategies complexes)
+		square_strategy()
+		...
 	'''
 	
 	def __init__(self, robot):
 		"""
-			prend le robot en param
+			prend le robot en parametre (possibilité d'utiliser un proxy comme intermediaire)
 		"""
 		
 		self.robot = robot
@@ -20,6 +33,7 @@ class Controleur:
 		"""
 			mise a jour du comportement et de l'etat du robot
 			en fonction de son etat precedent
+			sera appelée dans chaque stratégie
 		"""
 		
 		#a completer
@@ -38,18 +52,24 @@ class Controleur:
 
 	def setParcours(self):
 		"""
-			strategie de rotation du robot (degré de rotation) ?
+			?
 		"""
 		
 		#a completer
 		return 
 
 	def setTeteBaisse():
+		"""
+			?
+		"""
 		
 		return
 	
 
 	def setSuivreBalise():
+		"""
+			?
+		"""
             #plus tard, bien plus tard
             
 		return
